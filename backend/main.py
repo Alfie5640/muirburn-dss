65b31f8240ce761aa6630de27b2b39faec0da2fe
Alfie5640 <alfiewalker5640@gmail.com>
2026-07-28 22:08:55 +0100

    config file for maintainability

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+import datetime
+import yaml
 
 app = FastAPI()
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.get("/season")
+def check_in_season() -> bool:
+    currTime = datetime.datetime.now()
+    with open('rules/muirburn_code_2026.yaml') as f:
+        data = yaml.load(f, Loader=yaml.SafeLoader)
+
+    start_of_season = data["season"]["standard"]["start_month_day"] #09-15
+    end_of_season = data["season"]["standard"]["end_month_day"] #03-31
+    return True
